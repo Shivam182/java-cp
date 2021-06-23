35647831f93bb0b7b9fec7bebeb73579bf8fbde1
Shivam <shivam@gmail.com>
2021-06-23 13:02:44 +0530

    rough class : Testing Purpose

---
+++ basicProgramming/src/TestClass.java
@@ -0,0 +1,44 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+
+public class TestClass {
+    public static void main(String[] args) {
+        // int n1 = 0;
+        // int a = 2 + n1;
+        // int d = 1;
+        // int d1 = 3;
+        // int t = 1;
+        // int checkPoint = n1;
+        // for (int i = n1; i < 10; i++) {
+
+        // if (i == checkPoint || i == n1) {
+        // System.out.print("at t = " + i + " val is --> ");
+        // checkPoint = a + ((t - 1) * (2 * d1 + (t - 2) * d)) / 2;
+        // System.out.println(checkPoint);
+        // t++;
+        // }
+
+        // }
+
+        // List<Long> list = new Vector<>();
+
+        // list.add(1l);
+        // list.add(2l);
+        // list.add(3l);
+        // list.add(1l);
+        // // do not initialize iterator before adding elements to list : ConcurrentModificationException
+        // Iterator<Long> iterator = list.iterator();
+
+        // while (iterator.hasNext()) {
+        //     long m = iterator.next();
+        //     System.out.println(list.indexOf(m));
+        // }
+
+        
+
+
+    }
+}
